bbbc0d7cdd4e641c08ac38ce8f803854220a8bbc
mustafagny <47952077+mustafagny@users.noreply.github.com>
2020-07-21 11:02:29 +0300

    Neominal:v1

---
+++ mail.php
@@ -0,0 +1,54 @@
+<?php
+
+    // POST Request
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+			$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $num = trim($_POST["num"]);
+        $sub = trim($_POST["sub"]);
+        $comment = trim($_POST["comment"]);
+
+        // Check sent to the mailer.
+        if ( empty($name) OR empty($num) OR empty($sub) OR empty($comment) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Please Fillup the form and try again.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        $recipient = "extrat420@gmail.com";
+
+        // Set the email sub.
+        $sub = "Mail contact from $name";
+
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Number: $num\n\n";
+        $email_content .= "Subject: $sub\n\n";
+        $email_content .= "Message:\n$comment\n";
+
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $num, $sub, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your comment has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your comment.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+
+?>
